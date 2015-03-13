81834cba31f9dc69b920ddb128d49b33ccb05a26
NiamhS <sweeneyniamh@gmail.com>
2015-03-13 11:27:53 -0400

    Created new var for my data and factored it out into datapedestrians.js file

---
+++ datapedestrians.js
@@ -0,0 +1,8 @@
+ //Created new var to hold my data called myPedestrianData
+      var myPedestrianData = [
+          ['Bronx', 54],
+          ['Brooklyn', 55],
+          ['Manhattan', 73],
+          ['Queens', 54],
+          ['Staten Island', 48]
+        ];
