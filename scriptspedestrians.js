c3c21390a12c50142f7bca640216a36d3b1e7eb7
NiamhS <sweeneyniamh@gmail.com>
2015-03-13 21:53:35 -0400

    Recommitting changes to scripts file

--- scriptspedestrians.js
+++ scriptspedestrians.js
@@ -1,12 +1,12 @@
 //Took textStyle code from google charts
 
- var myPedestrianTextStyle = {
-      fontName: 'Arial',
-      fontSize: 18,
-      bold: false,
-      italic: true,
-      color: 'gray',     // The color of the text.
-    }
+var myPedestrianTextStyle = {
+	fontName : 'Arial',
+	fontSize : 18,
+	bold : false,
+	italic : true,
+	color : 'gray',     // The color of the text.
+}
 
 // new var for manipulating gridlines
 var myGridLinesColor = {
@@ -14,45 +14,47 @@ var myGridLinesColor = {
 	count : 4
 };
 
+// Set chart options
+var options = {
+	'title' : 'Road Deaths: Percentage of Pedestrian Fatalities by Location',
+	'width' : 850,
+	'height' : 600
+};
+//end of options
 
-  // Set chart options
-        var options = {'title':'Road Deaths: Percentage of Pedestrian Fatalities by Location',
-                       'width':850,
-                       'height':600
-                       };//end of options
-        
-        //Created vAxis and hAxis objects in options
-        options.hAxis = {'title':'Borough'};
-        options.vAxis = {'title': 'Percent'};               
-        options.hAxis.textStyle = myPedestrianTextStyle              
-		options.vAxis.textStyle = myPedestrianTextStyle	
-		
-		
+//Created vAxis and hAxis objects in options
+options.hAxis = {
+	'title' : 'Borough'
+};
+options.vAxis = {
+	'title' : 'Percent'
+};
+options.hAxis.textStyle = myPedestrianTextStyle
+options.vAxis.textStyle = myPedestrianTextStyle
+
 options.vAxis.gridlines = myGridLinesColor;
 // this instructs vAxis gridlines to follow format contained within var myGridlinesColor
-			
-			
+
 // Load the Visualization API and the piechart package.
-      google.load('visualization', '1.0', {'packages':['corechart']});
+google.load('visualization', '1.0', {
+	'packages' : ['corechart']
+});
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
-      
-     
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Percentage of Total Road Fatalities');
-        data.addColumn('number', '');
-        data.addRows(myPedestrianData);
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
 
-      
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Percentage of Total Road Fatalities');
+	data.addColumn('number', '');
+	data.addRows(myPedestrianData);
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
